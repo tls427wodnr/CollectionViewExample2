8fcf6b963ef0106ebbe9b72f9e9b49c12513d8ab
tls427wodnr <tlswodnr427@naver.com>
2025-02-17 10:06:01 +0900

    Initial Commit

---
+++ CollectionViewExample2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewExample2
+//
+//  Created by tlswo on 2/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
